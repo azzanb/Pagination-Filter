e8b0a61b4e1fea6c4d7fdde9dc0adf1d97d39f43
Gaurav Nagar <gaurav.nagar@thetrainline.com>
2017-05-09 05:26:55 +0100

    Added search functionality

--- js/script.js
+++ js/script.js
@@ -3,6 +3,7 @@ const $studentsList = $('.student-list').children();
 const $paginationDiv = $('<div class="pagination"></div>')
 const ul = $('<ul></ul>');
 const li = $('<li></li>');
+const $errorMessage = $('<h1 class="error-message">No student found!!!!');
 const pageSize = 10;
 
 //$($studentsList[1]).hide();
@@ -10,27 +11,37 @@ const pageSize = 10;
 const pageCount = Math.ceil($studentsList.length/pageSize);
 
 
-const appentPageLink = () => {
-        for(let i=1;i<=pageCount;i++) {    
+const appentPageLink = (totalPages) => {
+        for(let i=1;i<=totalPages;i++) {    
             let li = $('<li></li>');
-            let anchor = $('<a class="active" href="#">' + i + '</a>');
+            let anchor = $('<a href="#">' + i + '</a>');
             $(li).append(anchor);
             $(ul).append(li)    
             $paginationDiv.append(ul);
-            $paginationDiv.append(ul);
+            
             $('.student-list').append($paginationDiv);            
         }
 }
 
-const showPage = (pageNumber) => {
-    console.log('Showing page number: ' + pageNumber);
-    for(let i=0;i<$studentsList.length;i++) {
+const showPage = (pageNumber, studentList) => {
+    
+    const allPages = $('li a');
+    const $currentPage = $(allPages[pageNumber]);
+
+    allPages.removeClass('active');
+    $currentPage.addClass('active');
+    if(studentList.length === 0){
+        $($errorMessage).show();
+        return;
+    }
+
+    for(let i=0;i<studentList.length;i++) {
         if(i >= (pageNumber * 10) && i <= (pageNumber * 10)+9){
-            $($studentsList[i]).show();
+            $(studentList[i]).show();
         }
         else
         {
-            $($studentsList[i]).hide();
+            $(studentList[i]).hide();
         }
     }
 }
@@ -38,16 +49,56 @@ const showPage = (pageNumber) => {
 const addEventListener = () => {
     const $pages = $('.pagination li');
     for(let i=0;i<pageCount;i++) {
-        $($pages)[i].click(function(){
-        showPage(i);
-    });
-    
+        $($pages[i]).click(function(){
+        showPage(i, $studentsList);
+    });    
     }
 }
 
-showPage(0);
-appentPageLink();
+//Search
+const $searchDiv = $('.page-header');
+const $div = $('<div class="student-search"></div>');
+const $searchInput = $('<input placeholder="Search for students...">');
+const $searchButton = $('<button>Search</button>');
+
+$div.append($searchInput).append($searchButton);
+$searchDiv.append($div);
+
+$searchButton.click(()=> {
+    $($errorMessage).hide();
+    let $searchText = $('input').val();
+    const pages = $('.pagination').remove();
+    let matchedStudents = [];
+
+    for(let i=0;i<$studentsList.length;i++) {
+        $studentDetail = $($studentsList[i]);
+         $($studentDetail).hide();
+        $studentName = $($studentDetail.find('h3')[0]);
+        $studentEmail = $($studentDetail.find('.email')[0]);
+        
+        
+        if($searchText === $studentName.text() || $searchText === $studentEmail.text()) {
+            matchedStudents.push( $studentDetail);
+        }       
+    }
+    
+     if(matchedStudents.length > 10) {
+            const totalPages = Math.ceil(matchedStudents.length/pageSize);
+            appentPageLink(pageCount);
+        }
+
+        showPage(0,matchedStudents);
+});
+
+
+const errorMessage = () => {
+    $searchDiv.append($errorMessage);
+}
+
+appentPageLink(pageCount);
 addEventListener();
+showPage(0, $studentsList);
+errorMessage();
 
 
 
